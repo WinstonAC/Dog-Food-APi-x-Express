9102f8d997b91e5b0f3b5a31758f037dd1c4060f
William <Billy.campbell17@gmail.com>
2020-11-13 15:45:06 -0500

    upades

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 let mongoURI = "";
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {touch 
   mongoURI = process.env.DB_URL;
 } else {
   mongoURI = "mongodb://localhost/recipe";
